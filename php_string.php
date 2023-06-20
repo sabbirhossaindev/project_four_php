c2666a7bbab91b61badeb633a3ac1e7160bbc8d1
sabbir hossain <mdsabbir477470@gmail.com>
2023-06-20 21:05:48 +0600

    string done

---
+++ php_string.php
@@ -0,0 +1,10 @@
+<?php
+$string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// echo $string[13];
+// echo $string[8];
+// echo $string[7];
+// echo $string[14];
+// echo $string[13];
+echo $string[-5];
+echo "\n";
+echo strlen($string);
